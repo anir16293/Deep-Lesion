8785a3d6bef042102de065651ec8d273289a5d19
Aniruddha <aniruddha16293@gmail.com>
2019-04-25 14:41:53 -0400

    Add files via upload

--- unzip.py
+++ unzip.py
@@ -11,3 +11,5 @@ for idx in range(1, 20):
     zip_ref = zipfile.ZipFile(full_fn)
     zip_ref.extractall(unzip_directory)
     zip_ref.close()
+    os.remove(full_fn)
+    print('Unzipped directory '+ str(idx)+ ' and deleted zip file')

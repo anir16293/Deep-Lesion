51e325c082f0dabeb27ff435f45ca7b2d284c9f7
Aniruddha <aniruddha16293@gmail.com>
2019-04-25 14:37:40 -0400

    Add files via upload

---
+++ unzip.py
@@ -0,0 +1,13 @@
+#unzip script
+
+import zipfile
+import os
+
+download_directory = ''
+unzip_directory = ''
+
+for idx in range(1, 20):
+    full_fn = os.path.join(download_directory,'Images_png_%02d.zip' % (idx+1))
+    zip_ref = zipfile.ZipFile(full_fn)
+    zip_ref.extractall(unzip_directory)
+    zip_ref.close()
